531b844a1da941b37d3e9d4e53f81e2921bc7616
Terral Shrewsbury <dantl09@live.com>
2025-01-15 17:48:32 -0700

    work from 1/15/2025

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
 /* ******************************************
  * This server.js file is the primary file of the 
- * application. It is used to control the project.
+ * application. It is used to control the project. 
+ * Other names for it can be index.js or app.js
  *******************************************/
 /* ***********************
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "layouts/layout") // not at views root
+
+
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)
+
+/*Index route*/
+app.get("/", function(req, res) {
+  res.render("index", { title: "Home" })
+})
+
 
 /* ***********************
  * Local Server Information
